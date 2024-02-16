cbc8b7d6b38736fcc0457c88a4ab36b47fadebd7
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-16 13:27:00 +0530

    learning useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React, useState } from "react";
 
 function App() {
+  // const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0); // useState hook, as we can't update variable (count) directly, 0 is initial value
+
+  function updateState() {
+    // setCount(count + 5);
+    setCount(count + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Learning usestate {count} times</h1>
+      <button onClick={updateState}>Click Me</button>
+    </>
   );
 }
 
